15eb3f04b0a758f184ae4a428db09bc414bab4f2
jamesstoels <james.stoels@gmail.com>
2020-02-13 15:40:01 +0100

    Update tab1.page.ts

--- handtekeningApp/src/app/tab1/tab1.page.ts
+++ handtekeningApp/src/app/tab1/tab1.page.ts
@@ -7,6 +7,10 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
-
+  points = [];
+  signatureImage;
+  showImage(data) {
+    this.signatureImage = data;
+  }
+ 
 }
