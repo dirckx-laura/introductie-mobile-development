7d8829e307dd4465af56e9c569d6dcb44970fe3a
jamesstoels <james.stoels@gmail.com>
2020-02-13 15:39:50 +0100

    Update tab1.module.ts

--- handtekeningApp/src/app/tab1/tab1.module.ts
+++ handtekeningApp/src/app/tab1/tab1.module.ts
@@ -5,6 +5,8 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab1Page } from './tab1.page';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
+import { SignaturePadModule } from '@ng-plus/signature-pad';
+
 
 @NgModule({
   imports: [
@@ -12,6 +14,7 @@ import { ExploreContainerComponentModule } from '../explore-container/explore-co
     CommonModule,
     FormsModule,
     ExploreContainerComponentModule,
+    SignaturePadModule,
     RouterModule.forChild([{ path: '', component: Tab1Page }])
   ],
   declarations: [Tab1Page]
