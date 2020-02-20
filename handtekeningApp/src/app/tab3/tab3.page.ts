ad93f87348beffd1567bb35f6ce9f831e56a038f
Abdel <s109434@ap.be>
2020-02-20 17:20:40 +0100

    random commit

--- handtekeningApp/src/app/tab3/tab3.page.ts
+++ handtekeningApp/src/app/tab3/tab3.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 
 @Component({
@@ -14,12 +15,37 @@ export class Tab3Page {
   passwordShown:boolean = false;
   passwordType:string = "password";
   
+  
+
+  constructor(public router: Router, public alertCtrl: AlertController) {}
+
+  async failNoPassAlert() {
+    const alert = await this.alertCtrl.create({
+    subHeader: 'ERR_N_PASS',
+    message: 'Het heeft echt een wachtwoord hoor...',
+    buttons: ['OK']
+   });
+   await alert.present(); 
+  }
 
 
-  constructor(private router: Router) {}
-
-    
-
+  async failPassAlert() {
+    const alert = await this.alertCtrl.create({
+    subHeader: 'ERR_F_PASS',
+    message: 'U bent niet ingelogd, fout wachtwoord.',
+    buttons: ['OK']
+   });
+   await alert.present(); 
+  }
+  
+  async presentAlert() {
+    const alert = await this.alertCtrl.create({
+    subHeader: 'Succes',
+    message: 'U bent ingelogd!',
+    buttons: ['OK']
+   });
+   await alert.present(); 
+  }
     public togglePass(){
       if (this.passwordShown) {
         this.passwordShown = false;
@@ -29,17 +55,21 @@ export class Tab3Page {
         this.passwordType = "text"; 
       }
     }
+  
     public logIn() {
       console.log(this.pass);
       if (this.pass === this.password) {
-        alert("U bent succesvol ingelogd!");
+        this.presentAlert();
+        console.log("ingelogd");
         this.router.navigateByUrl('/admin');
       }
         else if(this.pass.length == 0){
-          alert("Het heeft echt een wachtwoord hoor...");
+          this.failNoPassAlert();
+          console.log("Het heeft echt een wachtwoord hoor...");
         }
       else {
-          alert("Fout wachtwoord!");
+        this.failPassAlert();
+          console.log("Fout wachtwoord!");
           
       }
      
